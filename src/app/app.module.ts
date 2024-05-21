deec36c7e9b92d3a47662ac4a9cf420058c510f2
Cristian Varela Casas <cristianchantada@gmail.com>
2024-05-21 12:04:49 +0200

    curso Angular ejercicio final seccion 4

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { CounterModule } from './counter/counter.module';
+
+import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/components/hero/hero.component';
+import { ListComponent } from './heroes/components/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
